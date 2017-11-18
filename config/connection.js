1f6a6620358839fbfce9ad3986c4eb4f1812810f
Carmen Stockberger <carmen.stockberger@gmail.com>
2017-11-18 08:51:46 -0800

    udpated connection with JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,30 @@
 var mysql = require('mysql')
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "burgers_db"
+  })
+}
+
+var connection = mysql.createConnection({
+
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
+connection.connect();
 // Export connection for the ORM to use.
 module.exports = connection;
